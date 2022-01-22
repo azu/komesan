147967ed38123a7b651e03a3e62f317a88bd3c18
azu <azuciao@gmail.com>
2022-01-22 19:20:54 +0900

    updaet

--- app/routes/Twitter.ts
+++ app/routes/Twitter.ts
@@ -3,3 +3,20 @@ export type Tweet = {
     text: string;
     created_at: string;
 };
+
+export const fetchTwitter = (_url: string) => {
+    const query = new URLSearchParams([
+        ["query", "nyc"],
+        ["tweet.fields", "text,created_at"]
+    ]).toString();
+    console.log(query);
+    return fetch("https://api.twitter.com/2/tweets/search/recent?" + query, {
+        headers: {
+            Authorization: `Bearer ${process.env.TWITTER_TOKEN}`
+        }
+    })
+        .then((res) => res.json())
+        .then((json) => {
+            return (json as any).data;
+        });
+};
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,24 +1,9 @@
 import { LoaderFunction, useLoaderData } from "remix";
 import { BookmarkSite } from "~/routes/Bookmark";
+import { fetchTwitter } from "~/routes/Twitter";
 
 const fetchHatenaBookmark = (_url: string) => {
     return fetch("https://b.hatena.ne.jp/entry/json/http://www.hatena.ne.jp/").then((res) => res.json());
-};
-const fetchTwitter = (_url: string) => {
-    const query = new URLSearchParams([
-        ["query", "nyc"],
-        ["tweet.fields", "text,created_at"]
-    ]).toString();
-    console.log(query);
-    return fetch("https://api.twitter.com/2/tweets/search/recent?" + query, {
-        headers: {
-            Authorization: `Bearer ${process.env.TWITTER_TOKEN}`
-        }
-    })
-        .then((res) => res.json())
-        .then((json) => {
-            return (json as any).data;
-        });
 };
 export let loader: LoaderFunction = () => {
     return fetchTwitter("");
