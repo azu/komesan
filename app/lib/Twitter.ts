a26a0c39d247889b60a4b6be748d9b3b0ca41f01
azu <azuciao@gmail.com>
2022-01-22 21:35:45 +0900

    update

--- app/lib/Twitter.ts
+++ app/lib/Twitter.ts
@@ -3,12 +3,15 @@ export type Tweet = {
     id: string;
     text: string;
     created_at: string;
+    profile_image_url: string;
+    name: string;
 };
 
 export const fetchTwitter = (url: string, { TWITTER_TOKEN }: { TWITTER_TOKEN: string }) => {
     const query = new URLSearchParams([
         ["query", url],
-        ["tweet.fields", "text,created_at"]
+        ["tweet.fields", "text,created_at"],
+        ["user.fields", "profile_image_url,name,id"]
     ]).toString();
     return fetch("https://api.twitter.com/2/tweets/search/recent?" + query, {
         headers: {
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,46 +1,74 @@
 import { LoaderFunction, useLoaderData } from "remix";
 import { fetchTwitter, Tweets } from "~/lib/Twitter";
 import { BookmarkSite, fetchHatenaBookmark } from "~/lib/Bookmark";
+import { LinkItUrl } from "react-linkify-it";
 
 export let loader: LoaderFunction = async ({ context, request }) => {
     const url = new URL(request.url);
     const urlParam = url.searchParams.get("url");
     if (!urlParam) {
         return {
+            url: "",
             twitter: [],
             hatebu: undefined
         };
     }
     const TWITTER_TOKEN = context.TWITTER_TOKEN as string;
-    const [twitter, hatebu] = await Promise.all([
+    const [hatebu, twitter] = await Promise.all([
+        fetchHatenaBookmark(urlParam),
         fetchTwitter(urlParam, {
             TWITTER_TOKEN
-        }),
-        fetchHatenaBookmark(urlParam)
+        })
     ]);
     return {
+        url: /^https?:/.test(urlParam) ? urlParam : "",
         twitter,
         hatebu
     };
 };
 export default function Index() {
-    const { twitter, hatebu } = useLoaderData<{ twitter: Tweets; hatebu: BookmarkSite | undefined }>();
+    const { twitter, hatebu, url } =
+        useLoaderData<{ twitter: Tweets; hatebu: BookmarkSite | undefined; url: string }>();
+    console.log({ twitter, hatebu, url });
     return (
         <div>
+            <h1>Komesan</h1>
+            <a href={url}>{url}</a>
             <h2>はてなブックマーク</h2>
-            <ul>
+            <ul style={{ listStyle: "none" }}>
                 {hatebu?.bookmarks.map((bookmark) => {
                     return (
                         <li key={bookmark.user + bookmark.comment}>
-                            {bookmark.user}: {bookmark.comment}
+                            <img
+                                width="16"
+                                height="16"
+                                src={`https://cdn.profile-image.st-hatena.com/users/${bookmark.user}/profile.png`}
+                                alt={""}
+                                loading={"lazy"}
+                                style={{
+                                    paddingRight: "2px"
+                                }}
+                            />
+                            <span
+                                style={{
+                                    color: "#4B4B4B"
+                                }}
+                            >
+                                {bookmark.user}
+                            </span>
+                            : <LinkItUrl>{bookmark.comment}</LinkItUrl>
                         </li>
                     );
                 })}
             </ul>
             <h2>Twitter</h2>
-            <ul>
-                {twitter?.map((bookmark) => {
-                    return <li key={bookmark.id}>{bookmark.text}</li>;
+            <ul style={{ listStyle: "none" }}>
+                {twitter?.map((tweet) => {
+                    return (
+                        <li key={tweet.id}>
+                            <LinkItUrl>{tweet.text}</LinkItUrl>
+                        </li>
+                    );
                 })}
             </ul>
         </div>
