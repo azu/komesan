6f318876ba033bdaf5c28edf5521858df9f66d12
azu <azuciao@gmail.com>
2022-02-15 08:52:28 +0900

    Update Header

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,11 @@ export default function App() {
                 <meta name="viewport" content="width=device-width,initial-scale=1" />
                 <Meta />
                 <Links />
+                <script
+                    defer
+                    src="https://static.cloudflareinsights.com/beacon.min.js"
+                    data-cf-beacon='{"token": "e2cb9d3214dc4a358856fd26b8b72693"}'
+                />
             </head>
             <body>
                 <Outlet />
